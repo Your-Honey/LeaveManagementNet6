13c331a552edb159b3963462eca7360da78581e9
Gasharma <Gasharma@PTPL-LAP-142>
2022-03-15 18:03:41 +0530

    Asynchronous Database Repository

---
+++ LeaveManagement.Web/Repository/GenericRepository.cs
@@ -0,0 +1,57 @@
+﻿using LeaveManagement.Web.Contracts;
+using LeaveManagement.Web.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeaveManagement.Web.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private ApplicationDbContext db;
+
+        public GenericRepository(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+        public async Task<T> AddAsync(T entity)
+        {
+            await db.Set<T>().AddAsync(entity);
+            await db.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await GetAsync(id);
+            db.Set<T>().Remove(entity);
+          await  db.SaveChangesAsync();
+        }
+
+        public async Task<bool> Exists(int id)
+        {
+            
+            var entity = await GetAsync(id);
+            return entity != null;
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await db.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return null;
+            }
+            var entity = await db.Set<T>().FindAsync(id);
+            return entity;
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+             db.Set<T>().Update(entity);
+            await db.SaveChangesAsync();
+        }
+    }
+}
---
+++ LeaveManagement.Web/Repository/LeaveTypeRepository.cs
@@ -0,0 +1,15 @@
+﻿using LeaveManagement.Web.Contracts;
+using LeaveManagement.Web.Data;
+
+namespace LeaveManagement.Web.Repository
+{
+    public class LeaveTypeRepository : GenericRepository<LeaveType>, ILeaveTypeRepository
+    {
+
+        public LeaveTypeRepository(ApplicationDbContext db) : base(db)
+        {
+
+        }
+
+    }
+}
